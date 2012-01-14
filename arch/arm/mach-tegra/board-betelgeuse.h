aeb42f0730250af170bc3883a17cd603f1105096
Artem Makhutov <artem@makhutov.org>
2012-01-14 12:19:58 +0100

    Lets use 32bpp graphics

--- arch/arm/mach-tegra/board-betelgeuse.h
+++ arch/arm/mach-tegra/board-betelgeuse.h
@@ -39,8 +39,8 @@
 
 #define BETELGEUSE_GPU_MEM_SIZE 	SZ_128M			/* Memory reserved for GPU */
 
-#define BETELGEUSE_FB1_MEM_SIZE 	SZ_8M			/* Memory reserved for Framebuffer 1: LCD */
-#define BETELGEUSE_FB2_MEM_SIZE 	SZ_8M			/* Memory reserved for Framebuffer 2: HDMI out */
+//#define BETELGEUSE_FB1_MEM_SIZE 	SZ_16M			/* Memory reserved for Framebuffer 1: LCD */
+//#define BETELGEUSE_FB2_MEM_SIZE 	SZ_16M			/* Memory reserved for Framebuffer 2: HDMI out */
 
 #define DYNAMIC_GPU_MEM			0			/* use dynamic memory for GPU */
 
