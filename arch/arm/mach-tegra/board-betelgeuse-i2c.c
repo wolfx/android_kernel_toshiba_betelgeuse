ef6948073e302ab3cd2ff90e3f09cb689ec9446d
Artem Makhutov <artem@makhutov.org>
2012-01-24 22:44:29 +0100

    Some I2C updates

--- arch/arm/mach-tegra/board-betelgeuse-i2c.c
+++ arch/arm/mach-tegra/board-betelgeuse-i2c.c
@@ -40,12 +40,18 @@ static struct tegra_i2c_platform_data betelgeuse_i2c1_platform_data = {
 	.adapter_nr	= 0,
 	.bus_count	= 1,
 	.bus_clk_rate	= { 100000, 0 },
+	.scl_gpio	= {TEGRA_GPIO_PC4, 0},
+	.sda_gpio	= {TEGRA_GPIO_PC5, 0},
+	.arb_recovery	= arb_lost_recovery,
 };
 
 static struct tegra_i2c_platform_data betelgeuse_i2c2_platform_data = {
 	.adapter_nr     = 1,
 	.bus_count      = 1,
 	.bus_clk_rate   = { 100000, 0 },
+	.scl_gpio	= {0, TEGRA_GPIO_PT5},
+	.sda_gpio	= {0, TEGRA_GPIO_PT6},
+	.arb_recovery	= arb_lost_recovery,
 };
 
 static struct tegra_i2c_platform_data betelgeuse_i2c3_platform_data = {
@@ -54,6 +60,9 @@ static struct tegra_i2c_platform_data betelgeuse_i2c3_platform_data = {
 	.bus_clk_rate   = { 80000, 0 },
 	//.is_slave       = true,
 	.slave_addr     = 0x8a,
+	.scl_gpio	= {TEGRA_GPIO_PBB2, 0},
+	.sda_gpio	= {TEGRA_GPIO_PBB3, 0},
+	.arb_recovery	= arb_lost_recovery,
 };
 
 /*
@@ -87,6 +96,9 @@ static struct tegra_i2c_platform_data betelgeuse_dvc_platform_data = {
 	.bus_count	= 1,
 	.bus_clk_rate	= { 100000, 0 },
 	.is_dvc		= true,
+	.scl_gpio	= {TEGRA_GPIO_PZ6, 0},
+	.sda_gpio	= {TEGRA_GPIO_PZ7, 0},
+	.arb_recovery	= arb_lost_recovery,
 };
 
 int __init betelgeuse_i2c_register_devices(void)
@@ -98,7 +110,7 @@ int __init betelgeuse_i2c_register_devices(void)
 
 	platform_device_register(&tegra_i2c_device1);
 	platform_device_register(&tegra_i2c_device2);
-	platform_device_register(&tegra_i2c_device3);
+	//platform_device_register(&tegra_i2c_device3);
 	platform_device_register(&tegra_i2c_device4);
 	
 	return 0;
