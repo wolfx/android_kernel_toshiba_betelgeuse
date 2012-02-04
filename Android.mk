09b5dbd591c90ec875bba949030a9a6e6004f32d
Nicolas Pinot <npinot@gmail.com>
2012-02-04 15:06:11 +0100

    Add modules copy to Android.mk
    Change-Id: Ic5502909fa2241e5290dc74611ef117648a20c6e

--- Android.mk
+++ Android.mk
@@ -8,23 +8,36 @@ ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),betelgeuse)
 include $(CLEAR_VARS)
 
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/kernel
-$(warning OUT $(TARGET_OUT_INTERMEDIATES) )
-$(warning TOP $(TOPDIR) )
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 KERNEL_SRC := $(LOCAL_PATH)
+MODULES_OUT := $(CURDIR)/$(TARGET_OUT)/lib/modules
+
 TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
 $(TARGET_PREBUILT_KERNEL):= build_kernel
+TARGET_COPY_MODULES:= build_kernel
+#TARGET_COPY_MODULES:= $(KERNEL_OUT)/arch/arm/boot/zImage
 
-$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP) $(TARGET_COPY_MODULES)
 	$(transform-prebuilt-to-target)
 
 $(KERNEL_OUT):
 	mkdir -p $(KERNEL_OUT)
+
+$(MODULES_OUT):
+	mkdir -p $(MODULES_OUT)
 
 $(KERNEL_CONFIG): $(LOCAL_PATH)/arch/arm/configs/tegra_betelgeuse_android_defconfig | $(ACP)
 	$(transform-prebuilt-to-target)
 
 $(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG)
 	$(MAKE) -C $(KERNEL_SRC) O=../../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(shell pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- INSTALL_MOD_STRIP=1
+
+$(TARGET_COPY_MODULES): $(MODULES_OUT) $(TARGET_PREBUILT_KERNEL)
+	$(eval _modules_files := $(shell find $(KERNEL_OUT) -name '*.ko'))
+	$(foreach _module_file, $(_modules_files), \
+		$(eval _dest_file := $(shell basename $(_module_file) )) \
+		$(shell cp -f $(_module_file) $(CURDIR)/$(TARGET_OUT)/lib/modules/$(_dest_file)) \
+	)
+
 endif
 endif
