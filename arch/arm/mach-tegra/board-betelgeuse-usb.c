e928469b4d6e0a4234d974d0568e598edf26b79d
Artem Makhutov <artem@makhutov.org>
2012-06-24 20:12:31 +0200

    Some experiments with EHCI

--- arch/arm/mach-tegra/board-betelgeuse-usb.c
+++ arch/arm/mach-tegra/board-betelgeuse-usb.c
@@ -99,7 +99,7 @@ static struct usb_phy_plat_data tegra_usb_phy_pdata[] = {
 static struct tegra_ehci_platform_data tegra_ehci_pdata[] = {
 	[0] = {
 			.phy_config = &utmi_phy_config[0],
-			.operating_mode = TEGRA_USB_OTG,
+			.operating_mode = TEGRA_USB_HOST,
 			.power_down_on_bus_suspend = 1,
 			.default_enable = true,
 	},
@@ -113,7 +113,8 @@ static struct tegra_ehci_platform_data tegra_ehci_pdata[] = {
 	[2] = {
 			.phy_config = &utmi_phy_config[1],
 			.operating_mode = TEGRA_USB_HOST,
-			.power_down_on_bus_suspend = 0,
+			.power_down_on_bus_suspend = 1,
+			.hotplug = 1,
 			.default_enable = true,
 	},
 };
